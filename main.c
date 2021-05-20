18e0ae7dff6bf7fe385415cb83cdfd862026dc81
Daniel Ramos <danielramosm99@gmail.com>
2021-05-19 19:02:29 -0500

    Simple program to get use to git

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    char name[100];
+    unsigned int age;
+
+    printf("Hello world\n");
+    printf("Name: ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Age: ");
+    scanf("%u", &age);
+    printf("You're going to turn %u years old\n", ++age);
+
+    printf("Hi, %s\n", name);
+
+    return 0;
+}
