19d4093cce7b1f75845b935435b973b778b167d0
Daniel Ramos <danielramosm99@gmail.com>
2021-05-20 11:22:34 -0500

    sum and subtract functions implemented

--- functions.h
+++ functions.h
@@ -1,12 +1,12 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-void sum(int a, int b){
-
+int sum(int a, int b){
+    return a + b;
 }
 
-void subtract(int ab, int b){
-    
+int subtract(int ab, int b){
+    return a - b;
 }
 
 #endif
